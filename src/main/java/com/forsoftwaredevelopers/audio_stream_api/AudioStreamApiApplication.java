b5bdc0c36cf7b80e361e5984677092fba10fbf02
Jordy Rodríguez <desdejorgefm@gmail.com>
2025-12-04 23:05:49 -0500

    Initial commit

---
+++ src/main/java/com/forsoftwaredevelopers/audio_stream_api/AudioStreamApiApplication.java
@@ -0,0 +1,13 @@
+package com.forsoftwaredevelopers.audio_stream_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioStreamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioStreamApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forsoftwaredevelopers/audio_stream_api/AudioStreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forsoftwaredevelopers.audio_stream_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioStreamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
