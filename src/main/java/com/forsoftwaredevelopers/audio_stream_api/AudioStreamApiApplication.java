3bcdb91ca985723d82ace34795efec00fb3b4fca
Jordy Rodríguez <desdejorgefm@gmail.com>
2025-12-04 23:08:24 -0500

    Gretting message to test DO

--- src/main/java/com/forsoftwaredevelopers/audio_stream_api/AudioStreamApiApplication.java
+++ src/main/java/com/forsoftwaredevelopers/audio_stream_api/AudioStreamApiApplication.java
@@ -2,12 +2,20 @@ package com.forsoftwaredevelopers.audio_stream_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AudioStreamApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AudioStreamApiApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, World from stream api 4sd!";
 	}
 
 }
